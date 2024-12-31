b7b2b797af7cabc8fffdd3d9c6ccf065e18c16bd
rluzzatt <luzzattoran@gmail.com>
2024-12-31 11:38:01 +0200

    simon cond

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,22 +1,61 @@
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomChosenColour = buttonColours[nextSequence()];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
 
-var selectorButton = ".btn." + randomChosenColour;
-var selectedButton = $(selectorButton);
+$("html").keydown(function (e) {
+    console.log(e.key);
+    if (!started) {
 
-selectedButton.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+      }
+});
 
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-audio.play();
 
-var gamePattern = []; 
 
-gamePattern.push(randomChosenColour);
+$(".btn").click(function (e) {
 
-function nextSequence(){
-    var randomNmber = Math.floor(Math.random() * 4);
-    return randomNmber;
+    var userChosenColour = this.id;
+
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+
+    animatePress(userChosenColour);
+
+    console.log(userClickedPattern);
+});
+
+
+function nextSequence() {
+
+    level++;
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+    playSound(randomChosenColour);
+
+    $("#level-title").text("Level " + level);
 }
 
+function playSound(name) {
+
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+
+    $("." + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("." + currentColour).removeClass("pressed")
+    }, 100);
+}
